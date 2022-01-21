ad4b1c7bcad8771ca7fc79f6dad2c10e5e59ff2f
Ian Cleaver <51103036+IanCleaver@users.noreply.github.com>
2022-01-20 23:28:01 -0500

    Updated environment & minor fixes
    Completed version of assignment 1.
    Fixed enviorment which now allows for successful compiling of code. Added some minor fixes which cleaned up output when printing tasks. Changed environment name to TaskManager since it is more fitting. Added option to mark tasks as incomplete.

---
+++ TaskManager/TaskManager/models/ToDo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.models
+{
+    public class ToDo
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                }
+            }
+        }
+
+        private DateTime deadline;
+        public DateTime Deadline
+        {
+            get
+            {
+                return deadline;
+            }
+            set
+            {
+                if (deadline != value)
+                {
+                    deadline = value;
+                }
+            }
+        }
+
+        private bool isCompleted;
+        public bool IsCompleted
+        {
+            get
+            {
+                return isCompleted;
+            }
+            set
+            {
+                if (isCompleted != value)
+                {
+                    isCompleted = value;
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string temp;
+            if(IsCompleted)
+            {
+                temp = "Completed";
+            }
+            else
+            {
+                temp = "Incomplete";
+            }
+
+            return $"{Name}\t {Description}\t\t\t {Deadline}\t {temp}";
+        }
+    }
+}
